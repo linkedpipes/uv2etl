882f5ce417364a2335c486f38c998d820b0357fc
Petr Škoda <skodapetr@gmail.com>
2016-06-19 10:47:31 +0200

    Add support for SparqlSelect

--- src/main/java/com/linkedpipes/etl/convert/uv/configuration/SparqlSelectConfig.java
+++ src/main/java/com/linkedpipes/etl/convert/uv/configuration/SparqlSelectConfig.java
@@ -2,8 +2,13 @@ package com.linkedpipes.etl.convert.uv.configuration;
 
 import com.linkedpipes.etl.convert.uv.pipeline.LpPipeline;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.List;
+import org.openrdf.model.IRI;
+import org.openrdf.model.Statement;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.SimpleValueFactory;
+import org.openrdf.model.vocabulary.RDF;
 
 /**
  *
@@ -12,9 +17,6 @@ import org.slf4j.LoggerFactory;
 @XStreamAlias("eu.unifiedviews.plugins.trandformer.sparqlselect.SparqlSelectConfig")
 class SparqlSelectConfig implements Configuration {
 
-    private static final Logger LOG
-            = LoggerFactory.getLogger(SparqlSelectConfig.class);
-
     private String targetPath = "/out.csv";
 
     private String query = "SELECT ?s ?p ?o WHERE { ?s ?p ?o }";
@@ -22,18 +24,28 @@ class SparqlSelectConfig implements Configuration {
     @Override
     public void update(LpPipeline pipeline, LpPipeline.Component component) {
 
-        // @DataUnit.AsInput(name = "input")
-        // @DataUnit.AsOutput(name = "output")
+        pipeline.renameInPort(component, "input", "InputRdf");
+        pipeline.renameOutPort(component, "output", "OutputFiles");
 
-        // @Component.InputPort(id = "InputRdf")
-        // @Component.OutputPort(id = "OutputFiles")
+        component.setTemplate(LpPipeline.BASE_IRI + "resources/components/t-sparqlSelect");
 
-        // http://plugins.linkedpipes.com/ontology/t-sparqlSelect#
-        // query
-        // fileName
+        final ValueFactory vf = SimpleValueFactory.getInstance();
+        final List<Statement> st = new ArrayList<>();
+        final IRI configuration = vf.createIRI("http://localhost/resources/configuration/t-sparqlSelect");
 
-        LOG.error("{} : Removed.", component);
-        pipeline.removeComponent(component);
+        st.add(vf.createStatement(configuration,
+                RDF.TYPE,
+                vf.createIRI("http://plugins.linkedpipes.com/ontology/t-sparqlSelect#Configuration")));
+
+        st.add(vf.createStatement(configuration,
+                vf.createIRI("http://plugins.linkedpipes.com/ontology/t-sparqlSelect#query"),
+                vf.createLiteral(this.query)));
+
+        st.add(vf.createStatement(configuration,
+                vf.createIRI("http://plugins.linkedpipes.com/ontology/t-sparqlSelect#fileName"),
+                vf.createLiteral(this.targetPath)));
+
+        component.setLpConfiguration(st);
     }
 
 }
