c0d195bb1954b2acf0d1d8f95acb0d2547f151b5
Petr Škoda <skodapetr@gmail.com>
2016-06-22 12:04:36 +0200

    Remove unused code

--- src/main/java/com/linkedpipes/etl/convert/uv/configuration/RdfMergerConfig_V1.java
+++ src/main/java/com/linkedpipes/etl/convert/uv/configuration/RdfMergerConfig_V1.java
@@ -2,8 +2,6 @@ package com.linkedpipes.etl.convert.uv.configuration;
 
 import com.linkedpipes.etl.convert.uv.pipeline.LpPipeline;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -11,9 +9,6 @@ import org.slf4j.LoggerFactory;
  */
 @XStreamAlias("eu.unifiedviews.plugins.transformer.rdfmerger.RdfMergerConfig_V1")
 class RdfMergerConfig_V1 implements Configuration {
-
-    private static final Logger LOG
-            = LoggerFactory.getLogger(RdfMergerConfig_V1.class);
 
     @Override
     public void update(LpPipeline pipeline, LpPipeline.Component component) {
